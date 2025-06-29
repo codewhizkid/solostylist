7f93e78421f2d8b965d983ac412bf7bd559f5c16
codewhizkid <roelruiz1986@gmail.com>
2025-06-29 04:50:50 -0400

    app
    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-// Import all the onboarding components
-import WelcomePage from './components/WelcomePage';
-import BrandingPage from './components/BrandingPage';
+// Onboarding components
+import WelcomePage        from './components/WelcomePage';
+import BrandingPage       from './components/BrandingPage';
 import StyleSelectionPage from './components/StyleSelectionPage';
-import AboutYouPage from './components/AboutYouPage';
-import ServicesPage from './components/ServicesPage';
-import AvailabilityPage from './components/AvailabilityPage';
-import SocialLinksPage from './components/SocialLinksPage';
+import AboutYouPage       from './components/AboutYouPage';
+import ServicesPage       from './components/ServicesPage';
+import AvailabilityPage   from './components/AvailabilityPage';
+import SocialLinksPage    from './components/SocialLinksPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/onboarding/branding" element={<BrandingPage />} />
-          <Route path="/onboarding/style" element={<StyleSelectionPage />} />
-          <Route path="/onboarding/about" element={<AboutYouPage />} />
-          <Route path="/onboarding/services" element={<ServicesPage />} />
-          <Route path="/onboarding/availability" element={<AvailabilityPage />} />
-          <Route path="/onboarding/socials" element={<SocialLinksPage />} />
-          {/* Add more routes here for dashboard, booking page etc. later */}
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/"                        element={<WelcomePage />} />
+        <Route path="/onboarding/branding"     element={<BrandingPage />} />
+        <Route path="/onboarding/style"        element={<StyleSelectionPage />} />
+        <Route path="/onboarding/about"        element={<AboutYouPage />} />
+        <Route path="/onboarding/services"     element={<ServicesPage />} />
+        <Route path="/onboarding/availability" element={<AvailabilityPage />} />
+        <Route path="/onboarding/socials"      element={<SocialLinksPage />} />
+        {/* TODO: add /dashboard, /booking pages later */}
+      </Routes>
+    </div>
   );
 }
 
